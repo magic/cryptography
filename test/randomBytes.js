fcb89c927dbd09dbef0913431d7dba3c03353a51
Jascha Ehrenreich <jascha@jaeh.at>
2018-03-13 17:48:46 +0100

    add tests for invalid args and byte arg

--- test/randomBytes.js
+++ test/randomBytes.js
@@ -1,10 +1,21 @@
+const { isString, isError } = require('types')
+
 const randomBytes = require('../src/randomBytes')
 
 const runs = process.env.RUNS || 100
 
 const fns = [
-  { fn: () => randomBytes() === randomBytes(), runs: 100, expect: false },
-  { fn: async () => { const b = await randomBytes(); return b.length }, expect: 66 },
+  { fn: async () => await randomBytes() === await randomBytes(), runs, expect: false },
+  { fn: async () => await randomBytes(), runs, expect: isString },
+  { fn: async () => await randomBytes(), runs, expect: t => t.length === 66 },
+  { fn: async () => await randomBytes(33), runs, expect: t => t.length === 66 },
+  { fn: async () => await randomBytes(44), runs, expect: t => t.length === 88 },
+  { fn: async () => await randomBytes(111), runs, expect: t => t.length === 222 },
+  // should error when passed a string, object or array
+  { fn: async () => { try { await randomBytes('not a number')} catch(e) { return e }}, runs, expect: isError },
+  { fn: async () => { try { await randomBytes({})} catch(e) { return e }}, runs, expect: isError },
+  { fn: async () => { try { await randomBytes([])} catch(e) { return e }}, runs, expect: isError },
+  { fn: async () => { try { await randomBytes(() => { return 321 })} catch(e) { return e }}, runs, expect: isError },
 ]
 
 module.exports = fns
