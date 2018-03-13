36ef8f22b98c266fdf0206cfe8a42be35c7dd2f9
Jascha Ehrenreich <jascha@jaeh.at>
2018-03-13 01:29:27 +0100

    randombytes excepts a byte argument

--- src/randomBytes.js
+++ src/randomBytes.js
@@ -1,7 +1,14 @@
 const crypto = require('crypto')
+const { isNumber } = require('types')
 
-const randomBytes = () => new Promise((resolve, reject) => {
-  crypto.randomBytes(33, (err, buf) => {
+
+const randomBytes = (byte = 33) => new Promise((resolve, reject) => {
+  byte = parseInt(byte, 10)
+  if (!isNumber(byte)) {
+    reject(new Error(`Invalid number of bytes passed to cryptography.randomBytes. ${byte}`))
+  }
+
+  crypto.randomBytes(byte, (err, buf) => {
     if (err) {
       reject(err)
     }
