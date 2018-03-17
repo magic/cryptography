3b0e8635d6cb3538bb57e77fd6549a9384d79ad5
Jascha Ehrenreich <jascha@jaeh.at>
2018-03-17 05:28:12 +0100

    remove console.log

--- test/range.js
+++ test/range.js
@@ -3,7 +3,7 @@ const range = require('../src/range.js')
 
 const fns = [
   { fn: () => range(0, 10), expect: isArray },
-  { fn: () => range(0, 9), expect: t => console.log(t, t.length) || t.length === 10 },
+  { fn: () => range(0, 9), expect: t => t.length === 10 },
   { fn: () => range(10, 0), expect: t => t[0] > t[1] },
   { fn: () => range(10), expect: t => t[0] > t[1] },
   { fn: () => range(10), expect: t => t.length === 10 },
