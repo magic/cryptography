2670ed8878b7168f59f55512078930e31bff59f8
Jascha Ehrenreich <jascha@jaeh.at>
2018-04-20 23:46:57 +0200

    format

--- src/uuid.js
+++ src/uuid.js
@@ -1,7 +1,8 @@
 const v4 = require('uuid/v4')
 const v5 = require('uuid/v5')
 
-const { isUUID, isString } = require('@magic/types')
+const is = require('@magic/types')
+const log = require('@magic/log')
 
 const { DNS, URL, OID, X500 } = v5
 
@@ -12,12 +13,14 @@ const namespaces = {
   X500,
 }
 
-const cleanUUID = id => isUUID(id) ? id : v4()
+const cleanUUID = id => (is.uuid(id) ? id : v4())
 
 const addNS = (key, uuid = v4()) => {
-  if (!isString(key)) {
-    console.error('cryptography.uuid.addNS: expected string as first argument, got: {key}')
-    return false
+  if (!is.string(key)) {
+    const msg = `uuid.addNS: expected string as first argument, got: ${key}`
+    const err = new Error(msg)
+    log.error(err)
+    return err
   }
 
   if (namespaces[key]) {
